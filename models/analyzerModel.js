a8d753523c3b5910e233827b01ae4eeb0a2b0d19
GDmail <GDmail91@gmail.com>
2017-04-15 13:55:42 +0900

    *add host min,max

--- models/analyzerModel.js
+++ models/analyzerModel.js
@@ -295,14 +295,20 @@ module.exports = {
 	 */
 	insertOutboundCall : function(context, key, host) {
 		return new Promise(function(resolved, rejected) {
-			var insert = [key, host.name, host.status, host.speed, host.speed];
+			var insert = [key, host.name, host.status, 
+				host.speed, host.speed, host.speed, 
+				host.speed, host.speed, host.speed];
 	        var sql = "INSERT INTO obc_table SET " +
 	            "`host_act_id` = ?, " +
 	            "`host_name` = ?, " +
 	            "`host_status` = ?, " +
 	            "`host_speed` = ?, " +
+	            "`host_high_speed` = ?, " +
+	            "`host_low_speed` = ?, " +
 	            "`host_count` = 1 " +
 	            "ON DUPLICATE KEY UPDATE " +
+	            "`host_high_speed` = GREATEST(`host_high_speed`, ?), " +
+	            "`host_low_speed` = LEAST(`host_low_speed`, ?), "+
 	            "`host_speed` = `host_speed` + ?, " +
 	            "`host_count` = `host_count` + 1";
 	        context.connection.query(sql, insert, function (err, rows) {
