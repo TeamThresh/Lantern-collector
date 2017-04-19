2514dc8febc923c85e909ac61ea52e3166f3d6a9
GDmail <GDmail91@gmail.com>
2017-04-19 20:24:51 +0900

    *add national country code and collect client IP # 참조 http://dev.maxmind.com/geoip/geoip2/geolite2/ # 참조 https://github.com/runk/node-maxmind#performance--benchmark

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -4,6 +4,7 @@
 
 var resourceModel = require('../models/resourceModel');
 var analyzeController = require('./analyzeController');
+var maxmind = require('maxmind');
 
 /**
  *
@@ -18,33 +19,58 @@ module.exports = {
             access_token: req.header('access-token'),
             dump: req.body
         };
-console.log("lanuch_time : "+data.dump.data.length);
 
-        // TODO access_token 으로 User 인증
-        // TODO validation 파싱을 controller로 뺄 것
-        resourceModel.saveResourceDump(data.dump, function(err, resource) {
+        // Client IP 가져옴
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        //(IPv6 to IPv4 format)
+        ip = ip.split(':'); 
+        data.dump.device_info.ip = ip[ip.length-1];
+
+        // City/Location lookup
+        maxmind.open('./GeoLite2-City.mmdb', (err, cityLookup) => {
             if (err) {
+                console.error(err);
                 return err instanceof Number ? next(err) : next(500);
             }
-            if (resource.data.length > 0) {
-                analyzeController.saveAnalysisDump(
-                    resource, 
-                    function(err) {
-                        if (err) {
-                            console.error(err);
-                            return err instanceof Number ? next(err) : next(500);
-                        }
-                        res.statusCode = 200;
-                        return res.json({
-                            msg: "complete"
+            // Lookup table에 국가 코드 조회
+            var city = cityLookup.get(data.dump.device_info);
+
+            // 국가코드, 국가 이름, 도시 이름
+            data.dump.device_info.location = {
+                code : city.country.iso_code,
+                country_name : city.country.names.en,
+                city_name : city.city.names.en
+            };
+
+            // 총 실행 횟수
+            console.log("lanuch_time : "+data.dump.data.length);
+
+            // TODO access_token 으로 User 인증
+            // TODO validation 파싱을 controller로 뺄 것
+            resourceModel.saveResourceDump(data.dump, function(err, resource) {
+                if (err) {
+                    return err instanceof Number ? next(err) : next(500);
+                }
+                if (resource.data.length > 0) {
+                    analyzeController.saveAnalysisDump(
+                        resource, 
+                        function(err) {
+                            if (err) {
+                                console.error(err);
+                                return err instanceof Number ? next(err) : next(500);
+                            }
+                            res.statusCode = 200;
+                            return res.json({
+                                msg: "complete"
+                            });
                         });
+                } else {
+                    res.statusCode = 200;
+                    return res.json({
+                        msg: "complete"
                     });
-            } else {
-                res.statusCode = 200;
-                return res.json({
-                    msg: "complete"
-                });
-            }
+                }
+            });
         });
     }
 };
